69361a6cd7d5cb9af5a2a44a60632416cdae6060
AFORANURAG <anuragupadhyay172912313@gmail.com>
2023-01-19 16:18:51 +0530

    authentication fully completed with typescript and shortner algo started

---
+++ backend/middlewares/authenticator.middleware.ts
@@ -0,0 +1,47 @@
+// we will require to import secret keys and other thing here
+import jsonwebtoken, { decode, Jwt } from "jsonwebtoken";
+import {SECRET_KEY} from "../secret";
+import express, { NextFunction } from "express"
+import  {Redis}  from "ioredis";
+import jwt_decode from "jwt-decode";
+
+let redis=new Redis()
+
+export const  authenticator=async (req:express.Request,res:express.Response,next:NextFunction):Promise<void>=>{
+
+console.log(req.cookies)
+    let Token=req.cookies?.accessToken
+let tokensfromredis=await redis.lrange("blacklistedusers",0,-1)
+console.log(tokensfromredis)
+try {
+if(tokensfromredis.includes(Token)){
+res.json({message:"You are logged out please login again"})
+
+}
+else{
+    interface accesstoken{
+        emailId:string,
+    }
+ let verification=jsonwebtoken.verify(Token,SECRET_KEY)
+
+let decoded=jwt_decode<accesstoken>(Token)
+
+if(decoded&&verification){
+
+//  console.log(decoded)
+ const {emailId}=decoded
+req.body.email=emailId
+console.log(emailId)
+ next()   
+}else{
+    res.status(401).json({message:"not authorize,Please login again"})
+}
+}  
+} catch (error) {
+ console.log(error)
+ res.status(500).json({message:"somewent wrong",error:error})   
+}
+
+}
+
+// now it is the time to create a oauth system.
---
+++ backend/secret.ts
@@ -0,0 +1,5 @@
+export const MONGO_URL="mongodb+srv://anurag:anuragupadhyay@cluster0.8epkihf.mongodb.net/unit-5CW?retryWrites=true&w=majority"
+ 
+export const  SECRET_KEY="ANURAG9801"
+export const REFRESHTOKEN=""
+export const  ACCESSTOKEN=""
