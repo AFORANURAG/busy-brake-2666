22e26d6abb6902e7cc68905db96bde06f649b235
AFORANURAG <anuragupadhyay172912313@gmail.com>
2023-01-20 23:44:32 +0530

    front completed and url shortner design completed

--- backend/models/shorturl.model.ts
+++ backend/models/shorturl.model.ts
@@ -3,12 +3,22 @@ interface shortUrl{
 full:string,
 short:string,
 click:number,
-initiallength:string,
-finallength:string
+initiallength:number,
+finallength:number
 }
+
 const shorturlSchema=new mongoose.Schema <shortUrl> ({
 full:{
-    
-}
+    type:String,
+    required:true
+},
+short:{
+    type:String,
+    required:true
+},
+click:{
+    type:Number,
+    required:true
+},
 
 })
