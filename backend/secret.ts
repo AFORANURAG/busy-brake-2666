71c2dbd268e354a44efffedab4b83f19774d1395
AFORANURAG <anuragupadhyay172912313@gmail.com>
2023-01-20 23:49:30 +0530

    front completed and url shortner design completed

--- backend/secret.ts
+++
@@ -1,8 +0,0 @@
-export const MONGO_URL="mongodb+srv://anurag:anuragupadhyay@cluster0.8epkihf.mongodb.net/unit-5CW?retryWrites=true&w=majority"
-export const  SECRET_KEY="ANURAG9801"
-export const REFRESHTOKEN=""
-export const  ACCESSTOKEN=""
-export const  googleclientid="172721478034-t9h00s6goa5c12ttem5fl6p5psmohs5r.apps.googleusercontent.com"
-export const googleclientsecret="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"
-export const githubclientsecret="4cee62b4423c80d7b652012f977c42a5718d1acd";
-export const githubclientid="e7aeef7786b473bf74b3"
